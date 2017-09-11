5a17d4100fe58523895bc416fdf8e0f7d770e445
Dmitriy Silitckas <dmitriy.silitckas@mediapark.com>
2017-09-11 13:38:16 +0200

    Changed .env.example + changed news seed

--- database/factories/NewsFactory.php
+++ database/factories/NewsFactory.php
@@ -11,7 +11,7 @@ use Faker\Generator as Faker;
 $factory->define(App\News::class, function (Faker $faker) {
     return [
         'title' => $faker->name,
-        'publish_date' => $faker->dateTime,
+        'publish_date' => $faker->dateTimeThisMonth($max = 'now', $timezone = date_default_timezone_get()),
         'preview_text' => $faker->text(),
         'detail_text' => $faker->text(500)
     ];
